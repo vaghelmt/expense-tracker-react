8b0298a1496772bc8134e7c2ace3d2cf9a4de31a
Mitul Vaghela <mitul.vaghela@gmail.com>
2022-01-31 16:31:58 -0500

    added date filter component and extracted expenses into a separate component named expense list

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,17 +2,18 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
-const ExpenseItem = (props)=> {
+const ExpenseItem = (props) => {
   return (
+    <li>
       <Card className="expense-item">
-          <ExpenseDate date={props.date}/>
-          <div className="expense-item__description">
-              <h2>{props.title}</h2>
-              <div className="expense-item__price">{props.amount}</div>
-          </div>
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">{props.amount}</div>
+        </div>
       </Card>
-
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,38 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditMode, setIsEditMode] = useState();
+
+  const editModeHandler = () => {
+    setIsEditMode(true);
+  };
+
+  const exitEditMode = () => {
+    setIsEditMode(false);
+  };
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
-    console.log(expenseData);
+    setIsEditMode(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditMode && (
+        <button onClick={editModeHandler}>Add New Expense</button>
+      )}
+      {isEditMode && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={exitEditMode}
+        />
+      )}
     </div>
   );
 };
